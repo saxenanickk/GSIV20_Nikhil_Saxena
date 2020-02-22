d6041c48e69e97d8f3cfbfd2f8d3967126f54ab3
Nikhil Saxena <saxenanickk@gmail.com>
2020-02-22 16:27:56 +0530

    store created

---
+++ src/store.js
@@ -0,0 +1,26 @@
+import {createStore, applyMiddleware, compose} from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+import reducer from './reducer';
+import rootSaga from './saga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+var middlewares = [sagaMiddleware];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+if (__DEV__) {
+  middlewares = [...middlewares, logger];
+}
+
+const store = createStore(
+  reducer(),
+  composeEnhancers(applyMiddleware(...middlewares)),
+);
+
+sagaMiddleware.run(rootSaga);
+
+console.log(store.getState());
+
+export default store;
