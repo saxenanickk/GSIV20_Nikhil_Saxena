9a80527e7b5c53daadc5764e99d3588aa09f994a
Nikhil Saxena <saxenanickk@gmail.com>
2020-02-22 16:29:51 +0530

    Provider added to whole application

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import MovieListing from './src/Containers/MovieListing';
 import MovieDetails from './src/Containers/MovieDetails';
+import {Provider} from 'react-redux';
+import store from './src/store';
 
 console.disableYellowBox = true;
 
@@ -21,14 +23,16 @@ const Stack = createStackNavigator();
 export default class App extends React.Component {
   render() {
     return (
-      <SafeAreaView style={{flex: 1}}>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Movie Listing" component={MovieListing} />
-            <Stack.Screen name="Movie Details" component={MovieDetails} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaView>
+      <Provider store={store}>
+        <SafeAreaView style={{flex: 1}}>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="Movie Listing" component={MovieListing} />
+              <Stack.Screen name="Movie Details" component={MovieDetails} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaView>
+      </Provider>
     );
   }
 }
