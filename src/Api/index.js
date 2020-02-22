e1764f92d46daa65e279d6af0e40d210dc3b5243
Nikhil Saxena <saxenanickk@gmail.com>
2020-02-22 16:29:12 +0530

    getUpcomingMovies api added

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,6 +1,26 @@
 import {SERVER_BASE_URL} from './SERVER_BASE_URL';
 import {THE_MOVIE_DB_API_KEY} from '../Config';
 
-const Api = {};
+const Api = {
+  getUpcomingMovies: ({page}) => {
+    let url = `${SERVER_BASE_URL}movie/upcoming?api_key=${THE_MOVIE_DB_API_KEY}`;
+
+    if (page) {
+      url = `${SERVER_BASE_URL}movie/upcoming?api_key=${THE_MOVIE_DB_API_KEY}&page=${page}`;
+    }
+
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(response => {
+          if (response.status === 200) {
+            response.json().then(res => resolve(res));
+          } else {
+            response.json().then(res => reject(res));
+          }
+        })
+        .catch(error => reject(error));
+    });
+  },
+};
 
 export default Api;
