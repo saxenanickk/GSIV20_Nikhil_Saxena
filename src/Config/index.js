181ead585d85389cdac1f46a3a046d1219380e10
Nikhil Saxena <saxenanickk@gmail.com>
2020-02-24 02:44:01 +0530

    comments in config

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,1 +1,7 @@
+/**
+ *  This is not the best way to handle this.
+ *  Must use Server to do this task.
+ *
+ *  So we only need to move this file and api file to server in order secure this.
+ */
 export const THE_MOVIE_DB_API_KEY = '610d659b1a19abd41925ed28f17e9bbd';
